c8c38d61a5c7b3a172dfd25ad2a536c4a57ead1c
Mert Çetin <mrtctn506@gmail.com>
2023-07-31 13:42:57 +0300

    basix structure created

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Note() {
+
+    const [students, setStudents] = useState([]);
+
     return (
-        <div>Note</div>
+        <div>
+            <input placeholder='Filter' />
+            <br />
+            <br />
+            <form>
+                <input name='name' placeholder='İsim gir' />
+                <input name='skor' placeholder='Notu gir' />
+                <button>Add</button>
+            </form>
+            <br />
+            <br />
+            <button>Sırala</button>
+
+        </div>
     )
 }
 
